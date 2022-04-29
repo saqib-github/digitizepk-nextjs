135f8a7369ec0f1778f593c98b66b59ec95feb1c
saqib <rsaqib2034@gmail.com>
2022-04-30 04:10:53 +0500

    work

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,48 +1,47 @@
-import nodemailer from "nodemailer";
-import sgTransport from "nodemailer-sendgrid-transport";
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
 
 let mailTransporter = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "30cc7a50ff58e0",
-    pass: "db4696c3651482",
-  },
+    service: 'gmail',
+    auth: {
+        user: 'saqib.browser@gmail.com',
+        pass: 'saqib786'
+    }
 });
 
 // const mailer = nodemailer.createTransport(sgTransport(transporter));
 
 export default async (req, res) => {
-  // console.log(req.body)
+    // console.log(req.body)
 
-  const { name, email, number, subject, text } = req.body;
-  console.log(req.body, "req body");
-  const mailDetails = {
-    // Update your email here
-    to: "saqib.browser@gmail.com",
-    from: `${email}`,
-    subject: "Hi there",
-    text: text,
-    html: `
+    const {name, email, number, subject, text} = req.body;
+    console.log(req.body, "req body")
+    const mailDetails = {
+        // Update your email here
+        to: 'saqib.browser@gmail.com',
+        from: `${email}`,
+        subject: 'Hi there',
+        text: text,
+        html: `
             <b>Name:</b> ${name} <br /> 
             <b>From/Email:</b> ${email} <br /> 
             <b>Number:</b> ${number} <br /> 
             <b>Subject:</b> ${subject} <br /> 
             <b>Message:</b> ${text} 
-        `,
-  };
-  try {
-    mailTransporter.sendMail(mailDetails, function (err, data) {
-      if (err) {
-        console.log("Error Occurs");
-      } else {
-        console.log("Email sent successfully");
-      }
-    });
-    // console.log(response)
-    res.status(200).send("Email send successfully");
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error proccessing charge");
-  }
-};
+        ` 
+    };
+    try {
+        mailTransporter.sendMail(mailDetails, function(err, data) {
+            if(err) {
+                console.log('Error Occurs');
+            } else {
+                console.log('Email sent successfully');
+            }
+        });
+        // console.log(response)
+        res.status(200).send("Email send successfully")
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error proccessing charge");
+    }
+}
