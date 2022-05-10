1085ac0f7630966773890e1f4d486c019bf1728a
saqib <rsaqib2034@gmail.com>
2022-05-11 01:04:18 +0500

    configure values changed for testing

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://digitizepk-nextjs-saqib-github.vercel.app' 
+? 'https://www.digitizepk.com' 
 : 'http://localhost:3000';
 
 export default baseUrl;
