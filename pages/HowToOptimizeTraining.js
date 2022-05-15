76d3ebf0d8425d6ca40dc6c1f99a853d2647deb9
saqib <rsaqib2034@gmail.com>
2022-05-15 18:38:23 +0500

    added title and meta description to blog pages

--- pages/HowToOptimizeTraining.js
+++ pages/HowToOptimizeTraining.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import Navbar from '../components/Layouts/Navbar';
-import PageBanner from '../components/Common/PageBanner';
-import HowToOptimizeTraining from '../components/News/HowToOptimizeTraining';
-import Footer from '../components/Layouts/Footer';
+import React, { Component } from "react";
+import Navbar from "../components/Layouts/Navbar";
+import PageBanner from "../components/Common/PageBanner";
+import HowToOptimizeTraining from "../components/News/HowToOptimizeTraining";
+import Footer from "../components/Layouts/Footer";
+import Head from "next/head";
 
 class NewsDetails extends Component {
-    render() {
-        return (
-            <>
-                <Navbar />
+  render() {
+    return (
+      <>
+        <Head>
+          <title>
+            How to Optimize Employee Training for a Remote workplace in 2022
+          </title>
+          <meta
+            name="description"
+            content="Remote developers for quick business assistance, proposed remote training mechanism. When COVID-19 hit in March 2020, companies quickly responded by switching over to a foreign workplace, allowing employees to figure from home to make sure their safety and wellbeing."
+          ></meta>
+        </Head>
+        <Navbar />
 
-                <PageBanner 
-                    pageTitle=" How to Optimize Employee Training for a Remote workplace in 2022" 
-                    homePageUrl="/" 
-                    homePageText="Blogs" 
-                    activePageText=" How to Optimize Employee Training for a Remote workplace in 2022" 
-                /> 
+        <PageBanner
+          pageTitle=" How to Optimize Employee Training for a Remote workplace in 2022"
+          homePageUrl="/"
+          homePageText="Blogs"
+          activePageText=" How to Optimize Employee Training for a Remote workplace in 2022"
+        />
 
-                <HowToOptimizeTraining />
-                
-                <Footer />
-            </>
-        );
-    }
+        <HowToOptimizeTraining />
+
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default NewsDetails;
